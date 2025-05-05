5aa9c1b094280a31f5173df39262006e684dd0f0
timkinsman <tkinsm@gmail.com>
2025-05-05 17:34:40 +0930

    chore: removed '@nayhoo/prettier-config' (causing build issues)

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,10 +1,10 @@
-import nayhooPrettierConfig from "@nayhoo/prettier-config";
-
-/**
- * @type {import("prettier").Config}
- */
 const config = {
-  ...nayhooPrettierConfig,
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-organize-imports"],
 };
 
 export default config;
